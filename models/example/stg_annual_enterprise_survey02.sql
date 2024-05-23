f1285eb4e7095c20dcc7367ae4613b8ccfc1c042
vasant1409 <149039305+vasant1409@users.noreply.github.com>
2024-05-23 20:02:01 +0530

    Update stg_annual_enterprise_survey02.sql

--- models/example/stg_annual_enterprise_survey02.sql
+++ models/example/stg_annual_enterprise_survey02.sql
@@ -5,16 +5,16 @@ SELECT * FROM `cloudside-academy.dataset1409.annual-enterprise-survey02`),
 x2 AS ( 
 SELECT 
 
-  t2_Year,
-  t2_Industry_aggregation_NZSIOC,
-  t2_Industry_code_NZSIOC,
-  t2_Units,
-  t2_Variable_code,
-  t2_Variable_name,
-  t2_Variable_category,
-  t2_Value,
-  t2_Industry_code_ANZSIC06,
-  t2_serial_number
+  Year as t2_Year,
+  Industry_aggregation_NZSIOC as t2_Industry_aggregation_NZSIOC,
+  Industry_code_NZSIOC as t2_Industry_code_NZSIOC,
+  Units as t2_Units,
+  Variable_code as t2_Variable_code,
+  Variable_name as t2_Variable_name,
+  Variable_category as t2_Variable_category,
+  Value as t2_Value,
+  Industry_code_ANZSIC06 as t2_Industry_code_ANZSIC06,
+  serial_number as t2_serial_number
 
 FROM survey
 )

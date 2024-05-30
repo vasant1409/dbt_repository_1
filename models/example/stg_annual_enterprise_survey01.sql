9fd0405b456fadde2c44f82bdb71a7b67438cc97
vasant1409 <149039305+vasant1409@users.noreply.github.com>
2024-05-30 12:28:30 +0530

    Update stg_annual_enterprise_survey01.sql

--- models/example/stg_annual_enterprise_survey01.sql
+++ models/example/stg_annual_enterprise_survey01.sql
@@ -2,12 +2,19 @@ WITH
 survey AS (
 SELECT * FROM `cloudside-academy.dataset1409.annual-enterprise-survey01`),
 
-ae AS ( 
+x AS ( 
 SELECT 
-    id,
-    value,
-    variable
+    id as t1_id,
+    year as t1_year,
+    industry_code_ANZSIC,
+    industry_name_ANZSIC,
+    rme_size_grp,
+    unit,
+    value t1_value,
+    variable,
+    serial_number as t1_serial_number,
+    serial_number_1 as t1_serial_number_1
 FROM survey
 )
 
-SELECT * FROM ae
+SELECT * FROM x

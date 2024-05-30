9a70e7e613a4f3adb15ec82dde1c947d6a72df8a
vasant1409 <149039305+vasant1409@users.noreply.github.com>
2024-05-30 12:29:29 +0530

    Update inter_annual_enterprise_survey.sql

--- models/example/inter_annual_enterprise_survey.sql
+++ models/example/inter_annual_enterprise_survey.sql
@@ -1,29 +1,48 @@
-with
-{{config(materialized='view')}}
+WITH
+{{config(materialized='ephemeral')}}
 
 inter_survey as (
 SELECT
-     SUM(CAST(t2.value AS INT64)) AS Total_Value,
-     {{ dbt_utils.star(from=ref('stg_annual_enterprise_survey01'), except=["id","value"]) }},
-     {{ dbt_utils.star(from=ref('stg_annual_enterprise_survey02'), except=["id","value"]) }}
-        
+     SUM(CAST(REPLACE(t2.Value,',', '') AS INT64)) AS Total_Value,
+     REPLACE(t2.Value,'C', ' ') AS Cleaned_Value,
+     {{ dbt_utils.star(from=ref('stg_annual_enterprise_survey01'))}},
+     {{ dbt_utils.star(from=ref('stg_annual_enterprise_survey02'))}}
 FROM
      {{ ref('stg_annual_enterprise_survey02') }} AS t2
 RIGHT JOIN
      {{ ref('stg_annual_enterprise_survey01') }} AS t1
 ON
-    t2.id = t1.id
+     t1.t1_serial_number = t2.t2_serial_number
+AND  t1.t1_serial_number_1 = t2.t2_serial_number_1
 WHERE
-    t2.Value <> 'C' AND t1.value <> 'C'
-GROUP BY
-      Year, 
-      Industry_code, 
-      Industry_name,  
-      Variable_name, 
-      Units,
-      variable,
-      Variable_category,
-      Industry_aggregation_NZSIOC
+     t1.t1_year = 2013
+AND  t2.t2_Year = 2013
+AND  t2.Value != 'C'  
+
+GROUP BY 
+     t1_id,
+     t2_ID,
+     t1_year,
+     t2_Year,
+     industry_code_ANZSIC,
+     industry_name_ANZSIC,
+     rme_size_grp,
+     unit,
+     t2.Value,
+     t1_value,
+     variable,
+     Industry_code,
+     Industry_name, 
+     Variable_name, 
+     Industry_code_ANZSIC06,
+     Units,
+     Variable_code,
+     Variable_category,
+     Industry_aggregation_NZSIOC,
+     t1_serial_number_1,
+     t1_serial_number,
+     t2_serial_number,
+     t2_serial_number_1
 )
 
-Select * from inter_survey
+SELECT * FROM inter_survey
